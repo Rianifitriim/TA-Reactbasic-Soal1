eacfbff748723061bb0970ca4fca3493e2872400
Rianifitriim <rianifitri53@gmail.com>
2021-10-24 21:47:37 +0700

    create soal 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import MovieCard from "./MovieCard";
+import animeList from "./dummy-data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container my-5">
+        <h1 className="text-center">Anime List</h1>
+        <div className="container my-5">
+          <div id="daftar-anime" className="row">
+            {
+              animeList.map(
+                (movie) => <MovieCard key={movie.mal_id} movie={movie} />)
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
